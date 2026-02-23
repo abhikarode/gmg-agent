6f929e36fc10f7d527716803f7b9703976502cd9
Abhijit Karode <abhi@MacStudioM4Max.local>
2026-02-22 16:30:06 -0800

    Fix auth.ts type error

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth";
+import NextAuth, { type NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-const config = {
+const config: NextAuthConfig = {
   providers: [
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID!,
